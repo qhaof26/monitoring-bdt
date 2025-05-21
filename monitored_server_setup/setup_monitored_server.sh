d74a52c66c5ec9dce92450886349b70bc02b706a
haogq <haogq@bigdatatech.vn>
2025-05-22 00:43:53 +0700

    fix(monitoring): update variables

--- monitored_server_setup/setup_monitored_server.sh
+++ monitored_server_setup/setup_monitored_server.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-
-# Cài đặt Node Exporter
-useradd -rs /bin/false node_exporter
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.0/node_exporter-1.8.0.linux-amd64.tar.gz
-tar xvf node_exporter-*.tar.gz
-cp node_exporter-*/node_exporter /usr/local/bin/
-
-cp node_exporter.service /etc/systemd/system/node_exporter.service
-systemctl daemon-reexec
-systemctl enable --now node_exporter
-
-# Cài đặt Promtail
-wget https://github.com/grafana/loki/releases/download/v2.9.2/promtail-linux-amd64.zip
-unzip promtail-linux-amd64.zip
-chmod +x promtail-linux-amd64
-mv promtail-linux-amd64 /usr/local/bin/promtail
-
-cp promtail-config.yaml /etc/promtail-config.yaml
-cp promtail.service /etc/systemd/system/promtail.service
-
-systemctl daemon-reexec
-systemctl enable --now promtail
+# Script khởi động node_exporter
+NODE_EXPORTER_VERSION="1.7.0"
+ARCH="linux-amd64"
+DOWNLOAD_URL="https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/node_exporter-$NODE_EXPORTER_VERSION.$ARCH.tar.gz"
+echo "Tải node_exporter..."
+wget $DOWNLOAD_URL -O node_exporter.tar.gz
+echo "Giải nén..."
+tar -xzf node_exporter.tar.gz
+mkdir -p ./node_exporter
+mv node_exporter-$NODE_EXPORTER_VERSION.$ARCH/* ./node_exporter/
+rm -rf node_exporter-$NODE_EXPORTER_VERSION.$ARCH node_exporter.tar.gz
+echo "Khởi chạy node_exporter..."
+nohup ./node_exporter/node_exporter > /dev/null 2>&1 &
+echo "Hoàn tất. Đang chạy tại port 9100."
